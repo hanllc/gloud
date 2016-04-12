b8f2c30e1343920c56c9c1f161c8109b3488bb0f
wehrli <joe@wehrli.org>
2016-04-12 01:29:58 -0400

    first pass hello world nginx config for brokeravm

--- Scripts/XsdK-1/XsdK-1-Install.sh
+++ Scripts/XsdK-1/XsdK-1-Install.sh
@@ -2,20 +2,26 @@
 apt-get -q -y -u update
 apt-get -q -y -u -V install nginx
 
-mkdir /pdata
-mkdir /pdata/web
-mkdir /pdata/web/xsdlive.com
+mkdir -p /pdata/web/xsdlive.com
 mkdir /pdata/web/brokeravm.com
 mkdir /pdata/web/wehrli.org
-
-#mkdir /etc/nginx/sites-available/brokeravm.com
-#wget brokeravm
+chown -R www-data:www-data /pdata/web/xsdlive.com
+chown -R www-data:www-data /pdata/web/brokeravm.com
+chown -R www-data:www-data /pdata/web/wehrli.org
+chmod 755 /pdata/web
 
 curl -o instance-config-key.asc "http://metadata.google.internal/computeMetadata/v1/instance/attributes/xsdkey"  -H "Metadata-Flavor: Google"
 curl -o brokeravm.com.asc https://raw.githubusercontent.com/hanllc/gcloud/master/Scripts/XsdK-1/brokeravm.com.asc
 gpg --allow-secret-key-import --import instance-config-key.asc
 gpg --output brokeravm.com --decrypt brokeravm.com.asc
 
+cp brokeravm.com /etc/nginx/sites-available/brokeravm.com
+sudo ln -s /etc/nginx/sites-available/brokeravm.com /etc/nginx/sites-enabled/brokeravm.com
+
+
+mv /etc/nginx/sites-enabled/default .
+
+service nginx restart
 
 
 
